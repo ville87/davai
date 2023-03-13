9efc7ba7bcacf7237238489fc290e8ac2a79f805
ville87 <villekoch87@gmail.com>
2023-03-13 10:01:49 +0200

    added new attack paths

--- AddVulnAzureADObjects.ps1
+++ AddVulnAzureADObjects.ps1
@@ -1,5 +1,6 @@
 # This script can be used to generate some AzureAD users which will be assigned with high privileges.
-# In addition, it creates new AzureAD Apps and registers Service Principals which will be assigned with high Graph API permissions
+# In addition, it creates new AzureAD Apps and registers Service Principals which will be assigned with 
+# high Graph API permissions and / or high Azure AD roles for privesc attack paths
 
 # Number of Azure AD users you want to create
 [int]$usercount = 10
@@ -14,26 +15,38 @@
 # Define dangerous Azure AD roles
 [array]$MostDangerousAzADRBACRoles = @("Global Administrator","Privileged Role Administrator","Privileged Authentication Administrator","Partner Tier2 Support")
 [array]$PotentiallyDangerousAzADRBACRoles = @("Application Administrator","Authentication Administrator","Azure AD joined device local administrator","Cloud Application Administrator","Cloud device Administrator","Exchange Administrator","Groups Administrator","Helpdesk Administrator","Hybrid Identity Administrator","Intune Administrator","Password Administrator","User Administrator","Directory Writers")
+Write-Host "Script started on $(Get-Date -Format 'dd/MM/yyyy HH:mm:ss')"
 
 # Import module AzureAD
 Try {
+    Write-Host "Importing module AzureAD..."
     Import-Module AzureAD -ErrorAction Stop
 }
 Catch {
     Write-Warning "Could not import module AzureAD, which is required. Error message:`r`n$($error[0].Exception)`r`nTerminating script..."
     Exit
 }
+# Import module Az
+Try {
+    Write-Host "Importing module Az..."
+    Import-Module Az -ErrorAction Stop
+}
+Catch {
+    Write-Warning "Could not import module Az, which is required. Error message:`r`n$($error[0].Exception)`r`nTerminating script..."
+    Exit
+}
+
 # ask for tenant domain
 $tenantDomain = Read-Host "Please provide the domain of the target tenant"
 # Connect to the tenant
 Write-Host "Connecting to the Azure tenant now, please login with a Global Admin"
-$ConnectAzAcc = Connect-AzureAD -TenantDomain $tenantDomain
-if($ConnectAzAcc.TenantDomain -ne $tenantDomain){
+$ConnectAzADAcc = Connect-AzureAD -TenantDomain $tenantDomain
+if($ConnectAzADAcc.TenantDomain -ne $tenantDomain){
     # something went wrong...
     Write-Warning "Something went wrong when connecting to the Azure tenant. It was not possible to check the connection context. Script will abort..."
     Exit
 }else{
-    Write-host "Connected to tenant $($ConnectAzAcc.Context.Tenant.Id)"
+    Write-host "Connected to tenant $($ConnectAzADAcc.Context.Tenant.Id)"
 }
 # Check that current user is global admin
 if((Get-AzureADDirectoryRole).DisplayName -notcontains "Global Administrator"){
@@ -112,7 +125,7 @@ foreach($GraphPermission in $DangerousGraphPermissions){
     # Get the user defined graph app role
     $AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
     # Create new AzureAD app
-    $AzureADApp = New-AzureADApplication -DisplayName "TestApp_$PermissionName"
+    $AzureADApp = New-AzureADApplication -DisplayName "TestApp_dvaad_$PermissionName"
     # Generate service principal for the app
     $AzureADAppSP = New-AzureADServicePrincipal -AppId $AzureADApp.AppId
     # Get the Graph Permission (app role) and assign it to the new SP
@@ -122,9 +135,78 @@ foreach($GraphPermission in $DangerousGraphPermissions){
         AppName = $($AzureADApp.DisplayName)
         SvcPrincipalId = $($AzureADAppSP.ObjectId)
         GraphPermission = $PermissionName
+        AzureADPermission = "N/A"
     }
     $SPExport += $data
 }
+
+# Connect to the tenant using Az module
+Write-Host "Connecting to the Azure tenant with the Az module now, please login with a Global Admin"
+$AzTenantId = (Get-AzureADTenantDetail).ObjectId
+$ConnectAzAcc = Connect-AzAccount -Tenant $AzTenantId
+# get subscriptions and ask for which one to use
+$AzSubs = Get-AzSubscription
+if($AzSubs.count -lt 1){
+    Write-Warning "No subscription was identified in this environment, cannot continue! Please create a subscription first and ensure you have access to it."
+    Exit
+}
+Write-host "The following subscriptions are available to the provided credentials:`r`n$AzSubs`r`n"
+$continue = $false
+do{
+    $AzSubIdChosen = Read-Host "Please insert the SubscriptionId for which you want to deploy the vulnerable apps"
+    try{
+        Set-AzContext -Subscription $AzSubIdChosen -ErrorAction Stop
+        $continue = $true
+    }catch{
+        Write-Host "Something went wrong... Try again."
+    }
+}while($continue -eq $false)
+Write-host "Collecting location information..."
+$AzLocations = Get-AzLocation
+Write-host "$($AzLocations.DisplayName -join "`r`n")"
+$ChosenLocation = Read-Host "Please choose which location from the list above you want to deploy the new resources to"
+
+########## Deploy Web App whose Managed Identity has Privileged Role Administrator Role ##########
+# Create new Resource Group with Az Web App
+Write-Host "Creating Resource Group..."
+$NewAzRG = New-AzResourceGroup -Name "RG_dvaad_$(((New-Guid) -split("-"))[0])" -Location "$ChosenLocation"
+# Create App Service Plan
+Write-Host "Creating App Service Plan..."
+$NewAzAppSvcPlan = New-AzAppServicePlan -ResourceGroupName "$($NewAzRG.ResourceGroupName)" -Name "dvaad_AppSvcPlan_$(((New-Guid) -split("-"))[0])" -Location "$ChosenLocation" -Tier "Free"
+# Create App Service
+Write-Host "Creating App Service..."
+$NewAzAppSvc = New-AzWebApp -ResourceGroupName "$($NewAzRG.ResourceGroupName)" -Name "dvaad-AppSvc-$(((New-Guid) -split("-"))[0])" -Location "$ChosenLocation" -AppServicePlan "$($NewAzAppSvcPlan.Name)"
+# Assign Managed Identity to web service
+Write-Host "Assigning Managed Identity to web app..."
+Set-AzWebApp -AssignIdentity $true -Name $($NewAzAppSvc.Name) -ResourceGroupName "$($NewAzRG.ResourceGroupName)"
+# Get the assigned identity
+$NewAzAppSvcPrincipalId = (Get-AzWebApp -Name $NewAzAppSvc.Name).Identity.PrincipalId
+# Collecting Role information in AD and assigning role to managed identity...
+$AzureADRoleDefs = Get-AzureADMSRoleDefinition
+$AzADPrivRoleAdminRole = $AzureADRoleDefs | Where-Object { $_.DisplayName -like "Privileged Role Administrator"}
+New-AzureADMSRoleAssignment -RoleDefinitionId $($AzADPrivRoleAdminRole.Id) -PrincipalId $NewAzAppSvcPrincipalId -DirectoryScopeId '/'
+# Add new user and assign with contributor rights to the new resource group
+$UserUPN = "dvaad-rgcontributor@$tenantDomain"
+$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$PWString = [System.Web.Security.Membership]::GeneratePassword(14,1)
+$PasswordProfile.Password = $PWString
+$NewAzUser = New-AzureADUser -AccountEnabled $true -DisplayName "dvaad rgcontributor" -UserPrincipalName $UserUPN -PasswordProfile $PasswordProfile -MailNickName "dvaadrgcontributor"
+New-AzRoleAssignment -ObjectId $($NewAzUser.ObjectId) -RoleDefinitionName "Contributor" -ResourceGroupName "$($NewAzRG.ResourceGroupName)"
+# add created user to export data
+$data = [PSCustomObject]@{
+    UserUPN = $UserUPN
+    UserPW = $PWString
+    UserRole = @("Contributor")
+}
+$UserExport += $data
+# add created app and service principal to export data
+$data = [PSCustomObject]@{
+    AppName = $($NewAzAppSvc.Name)
+    SvcPrincipalId = $NewAzAppSvcPrincipalId
+    GraphPermission = "N/A"
+    AzureADPermission = "Privileged Role Administrator"
+}
+$SPExport += $data
 
 # Export created users with passwords and roles
 $UserExport | ConvertTo-Json | Set-Content -Path $UserlistJSON
@@ -132,6 +214,6 @@ $UserExport | ConvertTo-Json | Set-Content -Path $UserlistJSON
 $SPExport | ConvertTo-Json | Set-Content -Path $SPlistJSON
 
 #### DONE ####
-Write-Host "Script done!"
 Write-Host "Users created in Azure AD, UPN and PWs exported to $UserlistJSON"
 Write-Host "Apps and Service Principals created in Azure AD, details exported to $SPlistJSON"
+Write-Host "Script finished on $(Get-Date -Format 'dd/MM/yyyy HH:mm:ss')"
--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # DVAAD
 Damn Vulnerable Azure AD   
-> **_NOTE:_**  Work in progress...    
 > :warning: **This script should not be used in production environments!**
 
 ## Description
 This script generates a number of AzureAD accounts and assigns them with high privileges.
-In addition, it registers Azure AD apps and service principals, which it assigns with high MS Graph API permissions.
+In addition, it registers Azure AD apps and service principals, which it assigns with high MS Graph API permissions and Azure AD permissions for Privilege Escalation paths.
+
+## Created Objects and Attack Paths
+- Creates a number of Azure AD users (default: 10, can be changed in script)
+- Assigns the users with randomly generated passwords
+- Assigns the users randomly with the following Azure AD Roles: "Global Administrator","Privileged Role Administrator","Privileged Authentication Administrator","Partner Tier2 Support","Application Administrator","Authentication Administrator","Azure AD joined device local administrator","Cloud Application Administrator","Cloud device Administrator","Exchange Administrator","Groups Administrator","Helpdesk Administrator","Hybrid Identity Administrator","Intune Administrator","Password Administrator","User Administrator","Directory Writers"
+- Creates two new AzureAD apps (TestApp_dvaad_$PermissionName) and assigns the Graph API roles "RoleManagement.ReadWrite.Directory","AppRoleAssignment.ReadWrite.All"
+- Creates a new resource group (RG_dvaad_xxxxxxxx)
+- Creates a new Azure Web App Service (dvaad-AppSvc-xxxxxxxx) in this RG and assigns it with a managed identity. This managed identity is assigned with the Azure AD role "Privileged Role Administrator"
+- Creates a new user (dvaad-rgcontributor@$tenantDomain) and assigns it with the Contributor RBAC role on the new resource group
